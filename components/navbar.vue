65a3d1126eb793b546e63835e1025cf9ce9032fc
Petko6 <petrprochazka06@email.cz>
2022-07-31 21:31:12 +0200

    installed DaisyUi + test

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    
+  <div class="navbar bg-base-200">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
   </div>
+  <div class="flex-none">
+    <button class="btn btn-wide btn-primary">
+      Sign in with Steam
+    </button>
+  </div>
+</div>
 </template>
